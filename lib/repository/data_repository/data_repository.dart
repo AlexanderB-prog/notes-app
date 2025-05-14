798114304ae1dcf373d8e73a1cb7d169f89420a2
alexb7777 <alexb7777@example.com>
2025-05-14 13:31:47 +0300

    add comment

--- lib/repository/data_repository/data_repository.dart
+++ lib/repository/data_repository/data_repository.dart
@@ -3,6 +3,7 @@ import 'package:notes/repository/data_repository/data_repository_interface.dart'
 import 'package:notes/repository/data_repository/data_repository_isar.dart';
 
 DataRepositoryInterface dataRepository() {
+  /// Isar в web  не работает, добавил возможность внедрения другой базы для web
   if (kIsWeb) {
     return DataRepositoryIsar();
   } else {
